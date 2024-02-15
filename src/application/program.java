d468c31f8e46ff27c3aeaaea88ca08f90cb49f8a
Allana Larissa <larissaleonel98@gmail.com>
2024-02-15 17:06:36 -0300

    Checkmate logic

--- src/application/program.java
+++ src/application/program.java
@@ -19,7 +19,7 @@ public class program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true){
+        while (!chessMatch.getCheckMate()){
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -44,7 +44,10 @@ public class program {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
+
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
         }
     }
 
